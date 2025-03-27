20d678dff586daebae0882c31e5b7f036c3dd4f4
411211163yoyo <yc.n3x9@gmail.com>
2025-03-27 10:33:03 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>周攸晨's Python網頁</h1>"
+     homepage = "<h1>周攸晨's Python網頁(+8)</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
-    homepage += "<a href=/welcome?nick=yoyo>傳送使用者暱稱</a><br>"
+    homepage += "<a href=/welcome?nick=yoyo&work=pu>傳送使用者暱稱</a><br>"
     homepage += "<a href=/account>網頁表單傳值</a><br>"
     homepage += "<a href=/about>yoyo's簡介網頁</a><br>"
     return homepage
